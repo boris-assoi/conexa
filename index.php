7953955888180fcc395777aeb4bff3d045c5a3eb
Boris Assoi <boris.assoi@outlook.com>
2021-02-02 10:41:56 +0000

    Modification de la disposition des domaines

--- index.php
+++ index.php
@@ -131,59 +131,45 @@
             </div>
           </div>
           <div class="row multi-columns-row">
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="features-item">
                 <div class="features-icon"><span class="icon-lightbulb"></span></div>
                 <h3 class="features-title font-alt">Ideas and concepts</h3>
                 <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
               </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="features-item">
                 <div class="features-icon"><span class="icon-bike"></span></div>
                 <h3 class="features-title font-alt">Optimised for speed</h3>
                 <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
               </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="features-item">
                 <div class="features-icon"><span class="icon-tools"></span></div>
                 <h3 class="features-title font-alt">Designs &amp; interfaces</h3>
                 <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
               </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="features-item">
                 <div class="features-icon"><span class="icon-gears"></span></div>
                 <h3 class="features-title font-alt">Highly customizable</h3>
                 <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
               </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="features-item">
                 <div class="features-icon"><span class="icon-tools-2"></span></div>
                 <h3 class="features-title font-alt">Coding &amp; development</h3>
                 <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
               </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="features-item">
                 <div class="features-icon"><span class="icon-genius"></span></div>
                 <h3 class="features-title font-alt">Features &amp; plugins</h3>
-                <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
-              </div>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
-              <div class="features-item">
-                <div class="features-icon"><span class="icon-mobile"></span></div>
-                <h3 class="features-title font-alt">Responsive design</h3>
-                <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
-              </div>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
-              <div class="features-item">
-                <div class="features-icon"><span class="icon-lifesaver"></span></div>
-                <h3 class="features-title font-alt">Dedicated support</h3>
                 <p>Careful attention to detail and clean, well structured code ensures a smooth user experience for all your visitors.</p>
               </div>
             </div>
